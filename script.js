bdb1aaafcf69ad9da0964613a73f5b3334878767
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-09-08 00:22:04 +0200

    Do CSS final adjustements

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ const newBookButton = document.querySelector("#new-book-button");
 const newBookDialog = document.querySelector("#new-book-dialog");
 const closeDialog = document.querySelector("#close-dialog");
 const container = document.querySelector("#container");
+const totalBooksCount = document.querySelector("#total-books");
+const totalReadBooksCount = document.querySelector("#books-read");
+const totalBooksBackloggedCount = document.querySelector("#not-read");
+
+function countBooks(myLibrary) {
+    let totalBooks = 0;
+    let totalReadBooks = 0;
+    let totalBackloggedBooks = 0;
+    for (let i = 0; i < myLibrary.length; i++) {
+        totalBooks += 1;
+        if (myLibrary[i].read === "Read") {
+            totalReadBooks += 1;
+        } else {
+            totalBackloggedBooks += 1;
+        }
+    }
+    totalBooksCount.textContent = totalBooks;
+    totalReadBooksCount.textContent = totalReadBooks;
+    totalBooksBackloggedCount.textContent = totalBackloggedBooks;
+}
 
 const book1 = new Book("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 480, false);
 const book2 = new Book("The Fellowship of the Ring", "J. R. R. Tolkien", 423, true);
@@ -16,12 +36,13 @@ const book3 = new Book("The Final Empire: Mistborn", "Brandon Sanderson", 544, t
 let myLibrary = [book1, book2, book3]
 
 
-for (let i of myLibrary) {
-    createCard(i);
+for (let i = myLibrary.length - 1; i >= 0; i--) {
+    createCard(myLibrary[i]);
 }
+countBooks(myLibrary);
 
-let deleteButton = document.querySelectorAll(".delete-btn");
-let readStatusButton = document.querySelectorAll(".read-status"); 
+let deleteButton = document.getElementsByClassName("delete-btn");
+let readStatusButtonButton = document.querySelectorAll(".read-status"); 
 
 // ------- BOOK OBJECT -------
 
@@ -46,60 +67,100 @@ Book.prototype.changeReadStatus = function() {
 
 // ------- FUNCTIONS -------
 
-function updateNodeList() {
-    deleteButton.forEach((button) => {
-        button.removeEventListener('click', searchBookToDelete);
-    });
-    deleteButton = document.querySelectorAll(".delete-btn");
-    deleteButton.forEach(button => {
-        button.addEventListener("click", () => {
-            console.log("hey");
-            searchBookToDelete(myLibrary, button);
-        })
-    })
-
-    
-  }
-
 function addBookToLibrary(myLibrary) {
     const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
     myLibrary.unshift(newBook);
     createCard(newBook);
     console.log(myLibrary);
+    countBooks(myLibrary);
     myForm.reset();
-    updateNodeList();
-    
+   
 }
 
 function createCard(book) {
     let title = document.createElement("div");
     title.classList.add("title");
     title.textContent = book.title;
+
     let author = document.createElement("div");
     author.classList.add("author");
     author.textContent = `by ${book.author}`;
+
     let pages = document.createElement("div");
     pages.classList.add("pages");
     pages.textContent = `${book.pages} pages`;
-    let readStatus = document.createElement("button");
-    readStatus.textContent = book.read;
-    readStatus.classList.add("read-status");
-    readStatus.setAttribute("data-book", book.title);
+
+    let buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+
+    let readStatusButton = document.createElement("button");
+    readStatusButton.textContent = book.read;
+    readStatusButton.classList.add("read-status");
+    readStatusButton.setAttribute("data-book", book.title);
+    if (book.read === "Read") {
+        if (readStatusButton.classList.contains("btn-light-red")) {
+            readStatusButton.classList.remove("btn-light-red")
+        }
+        readStatusButton.classList.add("btn-light-green");
+    } else {
+        if (readStatusButton.classList.contains("btn-light-green")) {
+            readStatusButton.classList.remove("btn-light-green")
+        }
+        readStatusButton.classList.add("btn-light-red");
+    }
+
     let card = document.createElement("div");
     card.classList.add("card");
     card.setAttribute("data-book", book.title);
+
     let deleteButton = document.createElement("button");
     deleteButton.textContent = "DELETE";
     deleteButton.classList.add("delete-btn");
     deleteButton.classList.add("material-symbols-outlined");
     deleteButton.setAttribute("data-book", book.title);
+
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
-    card.appendChild(readStatus)
-    card.appendChild(deleteButton);
+    buttonContainer.appendChild(readStatusButton)
+    buttonContainer.appendChild(deleteButton);
+    card.appendChild(buttonContainer);  
     card.setAttribute("data-book", book.title);
     container.prepend(card);
+
+    readStatusButton.addEventListener("click", () => {
+        book.changeReadStatus();
+        readStatusButton.textContent = book.read;
+        if (book.read === "Read") {
+            if (readStatusButton.classList.contains("btn-light-red")) {
+                readStatusButton.classList.remove("btn-light-red")
+            }
+            readStatusButton.classList.add("btn-light-green");
+        } else {
+            if (readStatusButton.classList.contains("btn-light-green")) {
+                readStatusButton.classList.remove("btn-light-green")
+            }
+            readStatusButton.classList.add("btn-light-red");
+        }
+    })
+
+    readStatusButton.addEventListener("click", () => {
+        countBooks(myLibrary);
+    })
+
+    deleteButton.addEventListener("click", () => {
+        card.remove();
+        for (let item of myLibrary) {
+            if (item.title === deleteButton.dataset.book){
+                myLibrary.splice(myLibrary.indexOf(item), 1);
+            }
+        }
+        console.log(myLibrary);
+    })
+
+    deleteButton.addEventListener("click", () => {
+        countBooks(myLibrary);  
+    })
 }
 
 // ------- EVENTS -------
@@ -122,33 +183,4 @@ newBookDialog.addEventListener("click", (event) => {
 
 closeDialog.addEventListener("click", () => {
     newBookDialog.close();
-})
-
-function searchBookToDelete(myLibrary, button) {
-    for (let item of myLibrary) {
-        if (item.title === button.dataset.book) {
-            myLibrary.splice(myLibrary.indexOf(item), 1);
-            console.log(myLibrary);
-            button.parentElement.remove()
-        }
-    }
-}
-
-deleteButton.forEach(button => {
-    button.addEventListener("click", () => {
-        console.log("hey");
-        searchBookToDelete(myLibrary, button);
-    })
-})
-
-readStatusButton.forEach(button => {
-    button.addEventListener("click", () => {
-        for (let item of myLibrary) {
-            if (item.title === button.parentElement.dataset.book) {
-                item.changeReadStatus();
-                console.log(item.read);
-                button.textContent = item.read;
-            }
-        }
-    })
 })
